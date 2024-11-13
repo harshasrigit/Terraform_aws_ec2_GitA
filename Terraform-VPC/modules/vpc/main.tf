0bcdf0503282bc88d05693004888c9d0825bb63e
harshasrigit <165784972+harshasrigit@users.noreply.github.com>
2024-11-13 12:43:19 +0530

    Update main.tf

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -8,3 +8,17 @@ resource "aws_vpc" "my_vpc" {
     }
 }
 
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)  # Create subnets based on a list of CIDR blocks
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+# You could also output the subnet IDs like this
+output "subnet_ids" {
+  description = "List of subnet IDs"
+  value       = aws_subnet.subnets.*.id
+}
